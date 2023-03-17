1ce515f10ef0e4c069f082f0b39480857fdb24e5
Ramon Pinheiro <ramonzinhg12@gmail.com>
2023-03-16 21:37:09 -0300

    feat: added roboto font family

--- App.tsx
+++ App.tsx
@@ -1,15 +1,27 @@
 import { StatusBar } from "expo-status-bar";
 import { ThemeProvider } from "styled-components/native";
 
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+
 import { Groups } from "@screens/Groups/index";
 import theme from "@theme/theme";
+import { ActivityIndicator } from "react-native";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
   return (
     <ThemeProvider theme={theme}>
       <StatusBar style="light" translucent />
 
-      <Groups />
+      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
     </ThemeProvider>
   );
 }
